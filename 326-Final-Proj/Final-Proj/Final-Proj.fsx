b11fb1d0257b2d361dbc102d0593a7ef1be54dd9
Will Fraisl <willfraisl@Wills-MacBook-Pro.local>
2017-04-18 15:12:59 -0700

    Changed to Google Geo chart
    Decided to get the structure vs the data working first.  Did some
    research on libraries that could provide what we are looking for.
    Google Geo charts looks like what we are looking for.  Since it’s a
    prototype we could also just put in fake data

--- 326-Final-Proj/Final-Proj/Final-Proj.fsx
+++ 326-Final-Proj/Final-Proj/Final-Proj.fsx
@@ -1,56 +1,13 @@
-// Before running any code, invoke Paket to get the dependencies.
-//
-// You can either build the project (Ctrl + Alt + B in VS) or run
-// '.paket/paket.bootstrap.exe' and then '.paket/paket.exe install'
-// (if you are on a Mac or Linux, run the 'exe' files using 'mono')
-//
-// Once you have packages, use Alt+Enter (in VS) or Ctrl+Enter to
-// run the following in F# Interactive. You can ignore the project
-// (running it doesn't do anything, it just contains this script)
-(*
+//https://tahahachana.github.io/XPlot/chart/google-geo-chart.html
+
 #load "../../packages/FsLab/FsLab.fsx"
 
-open Deedle
-open FSharp.Data
 open XPlot.GoogleCharts
-open XPlot.GoogleCharts.Deedle
 
-// Connect to the WorldBank and access indicators EU and CZ
-// Try changing the code to look at stats for your country!
-let wb = WorldBankData.GetDataContext()
-let cz = wb.Countries.``Czech Republic``.Indicators
-let eu = wb.Countries.``European Union``.Indicators
-
-// Use Deedle to get time-series with school enrollment data
-let czschool = series cz.``School enrollment, tertiary, male (% gross)``
-let euschool = series eu.``School enrollment, tertiary, male (% gross)``
-
-// Get 5 years with the largest difference between EU and CZ
-abs (czschool - euschool)
-|> Series.sort
-|> Series.rev
-|> Series.take 5
-
-// Plot a line chart comparing the two data sets
-// (Opens a web browser window with the chart)
-[ czschool.[1975 .. 2010]; euschool.[1975 .. 2010] ]
-|> Chart.Line
-|> Chart.WithOptions (Options(legend=Legend(position="bottom")))
-|> Chart.WithLabels ["CZ"; "EU"]
-
-*)
-
-#I "../../packages/FSharp.Charting"
-#I "../../packages/Deedle"
-#load "FSharp.Charting.fsx"
-#load "Deedle.fsx"
-#load "../../packages/FsLab/FsLab.fsx"
-
-open System
-open Deedle
-open FSharp.Charting
-open XPlot.GoogleCharts
-open XPlot.GoogleCharts.Deedle
-
-
-let hmi = Frame.ReadCsv(__SOURCE_DIRECTORY__ + "/hmi.csv")
+let pop =
+  [ "Germany", 200; "United States", 300
+    "Brazil", 400;  "Canada", 500
+    "France", 600;  "RU", 700 ]
+pop
+|> Chart.Geo
+|> Chart.WithLabel "Popularity"
