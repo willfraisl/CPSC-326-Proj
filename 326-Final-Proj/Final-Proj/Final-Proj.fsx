52e88d3f8fb2d29f20998777c943d36c75a401d2
willfraisl <willfraisl@gmail.com>
2017-05-11 10:56:58 -0700

    Added comments

--- 326-Final-Proj/Final-Proj/Final-Proj.fsx
+++ 326-Final-Proj/Final-Proj/Final-Proj.fsx
@@ -1,26 +1,40 @@
-//https://tahahachana.github.io/XPlot/chart/google-geo-chart.html
+(*
+    CPSC 326 Final Project
+    By Will Fraisl, Blake Erickson
 
+    Our project generates a heatmap of US states that shows a score for each 
+    state. The score is generated from a mix of the housing price index and 
+    median household income for that state. States with a higher score show 
+    plases that are "easier" to live. Meaning the median household income is 
+    higher compared to the housing price index. Also to begin the program asks 
+    for user input in the console to choose a year to display.
+*)
+
+//Load in F# Labs
 #load "../../packages/FsLab/FsLab.fsx"
 
+//Open required libraries
 open System
 open Deedle
 open FSharp.Data
 open XPlot.GoogleCharts
 open XPlot.GoogleCharts.Deedle
 
+//Function to get year from user
 let getYear =
     Console.Write("Input year: ")
     Console.ReadLine()
 
-
+//Read income CSV file
 let income = Frame.ReadCsv("State_median_income.csv") |> Frame.indexRowsString("State")
 
-
+//Read hpi CSV file
 let hpi = Frame.ReadCsv("State_house_price_index.csv") |> Frame.indexRowsString("State")
 
-
+//Calculate state score
 let ratio = hpi.Zip(income, fun a b -> float(a)/float(b) * 1000.0)
 
+//Create google chart
 let chart =
     let options =
         Options(
